3bbcface5205c244f85fb37acd7866702809fcee
Dinesh Manchanda <dinesh2133@gmail.com>
2023-10-23 16:06:02 +0530

    About us test case 2

--- cypress/integration/examples/03_AboutUs.js
+++ cypress/integration/examples/03_AboutUs.js
@@ -22,23 +22,22 @@ it('About Us', function()
 
 )
 
-it('Closing About Us', function()
+it.only('Closing About Us', function()
 {
     cy.loginEnvironment();
     cy.get('.oxd-topbar-header-userarea').click();
     cy.get('a.oxd-userdropdown-link:contains("About")').click();
    cy.wait(3000);
-   //Element Class
-   //cy.get('.oxd-dialog-sheet')
-
-//By ROle attribute
-//cy.get('[role="document"]')
-
-//By Element Tag and Class
-   cy.get('div.oxd-dialog-sheet').should('be.visible');
    
+// Verify that the dialog box is initially visible
+//cy.get('div.oxd-dialog-sheet').should('be.visible');
+  
+// Close the dialog box
+cy.get('.oxd-dialog-close-button').click();
+//cy.wait(1000);
+// Verify that the dialog box is closed
+cy.get('div.oxd-dialog-sheet').should('not.exist');
    
-   cy.get('.oxd-dialog-close-button').click();
 
 }
 
